f4d156ce654cc54eb2c914c8db627bca425857f9
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2021-03-28 01:10:03 +0700

    Build a Predictor class with alternating signals

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
+class Predictor {
+  public:
+    Predictor() {};
+
+  public: 
+    std::vector<float> historicalData;
+
+  public:
+    bool predict(float newestData) {
+      historicalData.push_back(newestData);
+
+      return historicalData.size() % 2 == 0;
+    }
+};
 
 int main() {
-  std::string s;
-  while (std::cin >> s)
-    std::cout << 1 << std::endl;
+  float input;
+
+  Predictor predictor;
+  while (std::cin >> input) {
+    std::cout << predictor.predict(input) << std::endl;
+  }
 
   return 0;
 }
